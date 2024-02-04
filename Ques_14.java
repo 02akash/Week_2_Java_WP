cbf0bf743c2cf92575ad717117f21407a3493ffa
Akash soni <94229771+02akash@users.noreply.github.com>
2024-02-04 22:11:54 +0530

    Create Ques_14.java

---
+++ Ques_14.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Lecture10_2 {
+
+public static void main(String[] args) {
+    
+    ArrayList cart0 = new ArrayList();
+
+    cart0.add("apple");
+    cart0.add(100);
+    cart0.add('C');
+
+    ArrayList<Integer> cart2 = new ArrayList<Integer>();
+
+    cart2.add(100);
+    cart2.add(80);
+    // cart2.add("hi");
+    }
+
+}
