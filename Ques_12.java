a5f2f57f6fae96acbc6f5a5328991546605ff7a9
Akash soni <94229771+02akash@users.noreply.github.com>
2024-02-04 21:37:01 +0530

    Create Ques_12.java

---
+++ Ques_12.java
@@ -0,0 +1,14 @@
+
+class lec_12 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("hey buddy how old are you ?");
+        int age = sc.nextInt();
+        
+        System.out.println(" wowh! You have really grown up! you are " +age + " yeras old");
+    }
+
+    
+}
